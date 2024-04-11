a6587c4b39e0348b1356bdfc8df4a41aa6883bc8
672644@stud.hvl.no <672644@stud.hvl.no>
2024-04-11 12:34:43 +0200

    test
    test

---
+++ databaseEksempel/src/test/java/no/hvl/dat108/webshop/objects/ExpoAppTest.java
@@ -0,0 +1,21 @@
+package no.hvl.dat108.webshop.objects;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class ExpoAppTest {
+
+    
+/*
+ * https://stackoverflow.com/questions/49887939/what-is-the-use-of-contextloads-method-in-spring-boot-junit-testcases
+ */
+
+    
+
+    @Test
+    void contextLoads(){ }
+
+}
+
+
